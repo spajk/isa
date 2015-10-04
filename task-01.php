61a8c36327631a8d667418b1874050ce8e7ae115
Mateusz Marmołowski <git@spajk.com>
2015-10-04 18:29:09 +0200

    5 cwiczen na 1 dzien

--- task-01.php
+++ task-01.php
@@ -6,12 +6,22 @@
  */
 
 
-// ZADANIE - popraw ponizsze bledy, aby skrypt wykonal sie poprawnie i wy swietlil na ekranie "This is infoShare Academy 2015"
+// ZADANIE - popraw ponizsze bledy, aby skrypt wykonal sie poprawnie i wyswietlil na ekranie "123"
 
-$var-1 = 1;
-$var_2 = 2;
+$var1 = 1';
+
+$var-2 = 2;
+
+$Var3 = '3';
+
+$temp = '4s';
+$v4 += (int)'4s';
+
+$v5 = 2+3-'x1'
 
 
+// zmieniaj tylko kod NAD TYM komentarzem
 
+echo $var1.$var2.$var3.$v4.$v5;
 
 ?>
